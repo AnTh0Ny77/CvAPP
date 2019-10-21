9be6b7779327913c021d803a9c01aba480fec730
AnthoRoot <anthonybs.pro@gmail.com>
2019-10-21 19:22:40 +0200

    dynamic template &  request with cloud firestore

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import {ModalComponent} from '../modal/modal.component'
-import { SendstatusService } from "../services/sendstatus.service";
-
+import { SendstatusService } from "../services/sendstatus.service"
+import { DynaProfilService } from "../services/dyna-profil.service";
 
 @Component({
   selector: 'app-profil',
@@ -11,17 +11,30 @@ import { SendstatusService } from "../services/sendstatus.service";
 })
 export class ProfilComponent implements OnInit {
   modalDialogRef: MatDialogRef<ModalComponent>;
-
-  constructor(private dialog: MatDialog, private status : SendstatusService) { }
+  array:any;
   stats: boolean;
+  
+  constructor(private dialog: MatDialog, private status : SendstatusService,
+    private Dyna: DynaProfilService) { }
+  
   ngOnInit() {
     this.status.currentStatus.subscribe(status => this.stats = status );
+    this.Dyna.getProfile().subscribe(data => {
+      this.array = data.map(resp => {
+        return {
+          id: resp.payload.doc.id,
+          isEdit: false,
+          Name: resp.payload.doc.data()['name'],
+          Cover: resp.payload.doc.data()['cover'],
+          Profil: resp.payload.doc.data()['profil'],
+          Fb: resp.payload.doc.data()['fb'],
+          lk: resp.payload.doc.data()['lk'],
+          Github: resp.payload.doc.data()['github'],
+        };
+      }) 
+    }); 
   }
   openModal(){
     this.modalDialogRef = this.dialog.open(ModalComponent);
-  }
-  test(){
-    console.log(this.stats);
-  }
-  
+  } 
 }
--- src/app/services/dyna-profil.service.ts
+++ src/app/services/dyna-profil.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore } from "@angular/fire/firestore";
 @Injectable({
   providedIn: 'root'
 })
 export class DynaProfilService {
-
-  constructor() { }
+  profil: any;
+  constructor(private FireStore: AngularFirestore) { 
+  }
+  getProfile() { 
+    return this.FireStore.collection("profile").snapshotChanges();
+    ;
+    
+  }
 }
